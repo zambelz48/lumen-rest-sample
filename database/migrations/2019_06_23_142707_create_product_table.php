16fa49f3bf9efd535611da2b7dfe53a595c496b4
Nanda . J . A <nanda.julianda48@gmail.com>
2019-06-23 21:54:01 +0700

    Add product table migration

---
+++ database/migrations/2019_06_23_142707_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->uuid('_id')->primary();
+            $table->string('name', 200);
+            $table->decimal('price', 9, 2);
+            $table->string('image', 400);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
